4c9ac2b703dfda5dfa71a3d570b296eb9894199e
Andrey Korshunov <korandrey2@gmail.com>
2024-03-16 23:08:39 +0300

    добавил рандомизацию

--- src/main/java/ru/praktikumservices/qascooter/courier/CourierGenerator.java
+++ src/main/java/ru/praktikumservices/qascooter/courier/CourierGenerator.java
@@ -1,11 +1,13 @@
 package ru.praktikumservices.qascooter.courier;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class CourierGenerator {
 
     public static Courier valid() {
-        final String login = "kor10";
-        final String password = "kor10";
-        final String firstName = "kor10";
+        final String login = RandomStringUtils.randomAlphabetic(10);
+        final String password = RandomStringUtils.randomAlphabetic(10);
+        final String firstName = RandomStringUtils.randomAlphabetic(10);
         return new Courier(login, password, firstName);
     }
 
